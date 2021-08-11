0b9c54a5b4feb57bce85276d7ed77e4fbe55d7a4
asandelarvine <larvine.asande@student.moringaschool.com>
2021-08-11 12:20:36 +0300

    this commit will modify the file

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link href="index.css" rel="stylesheet" type="text/css">
+	<title>About</title>
+</head>
+<body class="abody">
+	<div id="topbar">
+		<div id="left">
+			<b>larv.Shots</b>
+		</div>
+		<div id="right">
+			
+				<a href="index.php" style="color: chartreuse;"><b>Home</b></a> 
+				<a href="about.php" style="color: chartreuse;"><b>About</b></a>
+				<a href="contacts.php" style="color: chartreuse;"><b>Contacts</b></a> 
+				<a href="help.php" style="color: chartreuse;"><b>Help</b></a>
+				<a href="signup.php" style="color: chartreuse;"><b>Sign Up</b></a>
+			    <a href="login.php" style="color: chartreuse;"><b>Log In</b></a>
+
+		
+		</div>
+	</div>
+	<div class="row">
+		<div class="column">
+		<h3>Family Photography</h3>
+		<p>These captures the relationship between parents,children,siblings and the extended family.<br>
+		Any family occasions is a golden chances to practice the art.<br>
+		Family photos not only show connection and tender moments caught between family members, but expresses authentic feeling.</p><img alt="" height="10%" src="./images/fmy.JPG" width="50%"> <img alt="" src=""></div>
+		<div class="column">
+		<h3>Portrait Photography</h3>
+		<p>Portraits tend to aim towards capturing a group or a persons personality.<br>
+		Apart from lighting and backdrops,poses contributes greatly to it's making.</p><img alt="" height="10%" src="./images/prt.jpg" width="30%"> <img alt="" height="10%" src="./images/prt1.jpg" width="30%"> <img alt="" height="10%" src="./images/prt3.jpg" width="25%"></div>
+		<div class="column">
+		<h3>Street photography</h3>
+		<p>Street photography is capturing scene in public places.<br>
+		It is administered for art or research that features empirical chance encounters and random<br>
+		occurrences.<br>
+		Subject matter doesn't always need to include people.<br>
+		It tends to be less deliberate in it's defined messaging.<br></p><img alt="" height="6%" src="./images/strt.JPG" width="49%"> <img alt="" height="10%" src="./images/strt.jpg" width="49%"></div>
+		<div class="column">
+		<h3>Wildlife Photography</h3>
+		<p>Of all kinds of photography,the more time one spends with their subject, the more likely ones'<br>
+		images will be intimate and revealing and for this kind of photography one needs to:<br></p>
+		<marquee width="60%" direction="right" height="100px">
+		<ul class="a"> 
+			<li>Be Patient</li>
+			<li>Have a Good gear</li>
+			<li>Be at a Safe shooting location</li>
+		</ul>
+	    </marquee>
+		<p></p><img alt="" height="10%" src="./images/wld.jpg" width="49%"> <img alt="" height="10%" src="./images/wld1.jpg" width="49%"></div>
+		<div class="column">
+			<h3>Candid Photography</h3>
+			<p>This is an unplanned moment.<br>
+			It removes the posed aspect of the equation,<br>
+			shots taken of people in motion,spontaneously or by surprise.<br>
+			Candid photography aims to capture subject in natural state without needing to direct or pose them.</p><img alt="" height="10%" src="./images/IMG-20210221-WA0054.jpg" width="49%"> <img alt="" height="700px" src="./images/cnd.jpg" width="49%">
+			<div class="column">
+			<h3>Golden-hour Photography</h3>
+			<p>This kind of photography is taken an hour before sunset or an hour after sunrise which<br>
+			the sun is at it's lowest point in the sky.<br>
+			With this it produces a warm golden tone as background lighting.</p><img alt="" height="10%" src="./images/gld2.jpg" width="49%"> <img alt="" height="10%" src="./images/gld6.jpg" width="49%"></div>
+		</div>
+	</div><br>
+	<br>
+	<div class="footer">
+		<p>@2021 copyright:LarvShots</p>
+	</div>
+</body>
+</html>
