af8b8c664ebb0bdccce35b3f7f879add1e0313ba
asandelarvine <larvine.asande@student.moringaschool.com>
2021-08-11 12:21:37 +0300

    this commit will add content to the file

---
+++ contacts.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Contacts</title>
+	<link href="index.css" rel="stylesheet" type="text/css">
+</head>
+<body id="cbody">
+	<div id="topbar">
+		<div id="left">
+			<b>LarvShots</b>
+		</div>
+		<div id="right">
+				<a href="index.php" style="color: chartreuse;"><b>Home</b></a> 
+				<a href="about.php" style="color: chartreuse;"><b>About</b></a>
+				<a href="contacts.php" style="color: chartreuse;" ><b>Contacts</b></a> 
+				<a href="help.php" style="color: chartreuse;"><b>Help</b></a>
+				<a href="signup.php" style="color: chartreuse;"><b>Sign Up</b></a>
+			    <a href="login.php" style="color: chartreuse;"><b>Log In</b></a>
+			</div>
+		</div>
+	</div>
+	<div id="rightside">
+		<address id="contacts">
+			<br>
+			<br>
+			<b><i>Contact us:</i></b><br>
+			P.O BOX 201,<br>
+			Thika Greens,<br>
+			Kenya.<br>
+			Contact:<br>
+			+254-745454545
+		</address>
+	</div>
+	<div class="footer">
+		<p>@2021 copyright:LarvShots</p>
+	</div>
+</body>
+</html>
