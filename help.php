fcf502ab42579d6f7c4c458200c7045c7628af12
asandelarvine <larvine.asande@student.moringaschool.com>
2021-08-11 12:22:36 +0300

    this commit will add content to the file

---
+++ help.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link href="index.css" rel="stylesheet" type="text/css">
+	<title>Help</title>
+</head>
+<body id="body2">
+	<div id="topbar">
+		<div id="left">
+			<b>larv.Shots</b>
+		</div>
+		<div id="right">
+			<a href="index.php" style="color: chartreuse;"><b>Home</b></a> 
+			<a href="about.php" style="color: chartreuse;"><b>About</b></a>
+			<a href="contacts.php" style="color: chartreuse;" ><b>Contacts</b></a> 
+			<a href="help.php" style="color: chartreuse;"><b>Help</b></a>
+			<a href="signup.php" style="color: chartreuse;"><b>Sign Up</b></a>
+			<a href="login.php" style="color: chartreuse;"><b>Log In</b></a>
+
+		</div>
+	</div><br>
+	<form action="">
+		<label for="fname">First name:</label><br>
+		<input id="fname" name="fname" type="text" value=""><br>
+		<label for="lname">Last name:</label><br>
+		<input id="lname" name="lname" type="text" value=""><br>
+		<label for="email">Email:</label><br>
+		<input id="email" name="email" type="text" value=""><br>
+		<Textarea>
+			""
+		</Textarea>
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+	<div class="footer">
+		<p>@2021 copyright:LarvShots</p>
+	</div>
+</body>
+</html>
